5d925f6f7a35ca8a6b7422da1de4660e48f2f6a1
Ruku <preciousubi.sam@gmail.com>
2022-12-12 12:47:08 -0800

    combinations 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible different combinations of two digits.
+ * main - Prints all possible different combinations of two digits
  * Return: 0
  */
 int main(void)
